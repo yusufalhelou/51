285d8ba7026826e0e5a87d2eb2d8669de9ab945d
yusufalhelou <108984728+yusufalhelou@users.noreply.github.com>
2025-01-23 02:38:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,24 @@
         hour = minute * 60,
         day = hour * 24;
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "01/23/",
-      birthday = dayMonth + yyyy + " 11:00:00";
-  
-  today = mm + "/" + dd + "/" + yyyy + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  if (today > birthday) {
-    birthday = dayMonth + nextYear + " 11:00:00";
-  }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
+  // Setting the specific countdown date to January 23, 2025, at 11 AM
+  const countDownDate = new Date("Jan 23, 2025 11:00:00").getTime();
 
-        const now = new Date().getTime(),
-              distance = countDown - now;
+  const x = setInterval(function() {
+    const now = new Date().getTime(),
+          distance = countDownDate - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+    document.getElementById("days").innerText = Math.floor(distance / (day));
+    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
+    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
+    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "It's done!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+    // Display a message when the countdown ends
+    if (distance < 0) {
+      document.getElementById("headline").innerText = "It's done!";
+      document.getElementById("countdown").style.display = "none";
+      document.getElementById("content").style.display = "block";
+      clearInterval(x);
+    }
+  }, 1000);
+}());
